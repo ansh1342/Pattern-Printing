f836b75d9fedb97c7da38410e01d5aae15f47d0f
ansh1342 <vermaansh1445@gmail.com>
2025-07-22 00:52:54 +0530

    New code

--- 08.cpp
+++ 08.cpp
@@ -1,5 +1,27 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int n;
+    cout << "Enter row: ";
+    cin >> n;
+    int i = 1;
+    while(i<=n/2){
+        int j = 1;
+        while(j<=i){
+            cout << "*";
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+    int k = (n/2) - 1;
+    while(k>0){
+        int m = 1;
+        while(m<=k){
+            cout << "*";
+            m++;
+        }
+        k--;
+        cout << endl;
+    }
 }
